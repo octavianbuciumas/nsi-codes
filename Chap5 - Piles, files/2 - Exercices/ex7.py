b2f3561393451bb09a24ebb104a33d1cb363924b
Florian Picard <florian.picard.fr@gmail.com>
2023-12-07 16:10:23 +0100

    ex7 tri crepe

--- Chap5 - Piles, files/2 - Exercices/ex7.py
+++ Chap5 - Piles, files/2 - Exercices/ex7.py
@@ -1,10 +1,10 @@
 from ds import *
 
 p = creer_pile_vide()
-empiler(p, 4)
+empiler(p, 8)
 empiler(p, 2)
 empiler(p, 5)
-empiler(p, 8)
+empiler(p, 10)
 
 
 def hauteur(pile):
@@ -21,7 +21,64 @@ def max_pile(p, i):
     position_max = 1
     valeur_max = depiler(p)
     # il faut utiliser une Pile auxiliaire
-    # à finir !
+    p_aux = creer_pile_vide()
+    empiler(p_aux, valeur_max)
+    pos = 1
+    while pos < i and not est_pile_vide(p):
+        pos += 1
+        elt = depiler(p)
+        if valeur_max < elt:
+            valeur_max = elt
+            position_max = pos
+        empiler(p_aux, elt)
+    while not est_pile_vide(p_aux):
+        empiler(p, depiler(p_aux))
+    return position_max
+    
+def retourner(p, i):
+    if est_pile_vide(p) or i > n:
+        raise IndexError("pb d'arguments")
+    n = hauteur(p)
+    f = creer_file_vide()
+    for _ in range(min(i, n)):
+        enfiler(f, depiler(p))
+    while not est_file_vide(f):
+        empiler(p, defiler(f))
+    return p
+    
+def tri_crepe(p):
+    n = hauteur(p)
+    for j in range(4, 1, -1):
+        position_max = max_pile(p, j)
+        retourner(p, position_max)
+        retourner(p, j)
+    affiche_pile(p)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
