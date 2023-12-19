86729bec4fa8f8488fc07c494ee54ca3905e83cf
Florian Picard <florian.picard.fr@gmail.com>
2023-12-19 17:34:24 +0100

    ordo fin

--- Chap6 - processus/3 - Ordonnancement/tp.py
+++ Chap6 - processus/3 - Ordonnancement/tp.py
@@ -8,6 +8,8 @@ class Processus:
         self.arrivee = arrivee
         self.duree = duree
         self.duree_restante = duree
+        self.debut = -1
+        self.fin = -1
         
     def __repr__(self):
         """ Processus -> str
@@ -17,28 +19,39 @@ class Processus:
     def avancer(self, d, t):
         """ Processus, int, int -> Nonetype
         Le processus (re)prend son exécution à la date t pour une durée d """
-        pass
+        assert self.arrivee <= t
+        # on enregistre la date du premier appel
+        if self.debut == -1:
+            self.debut = t
+        # on avance le processus
+        self.duree_restante -= d
+        # on enregistre l'instant de fin 
+        if self.est_termine():
+            self.fin = t + d 
     
     def est_termine(self):
         """ Processus -> bool
         Détermine si le processus p a terminé son exécution """
-        pass    
+        return self.duree_restante <= 0
+        # if self.duree_restante <= 0:
+        #     return True
+        # return False
     
     def sejour(self):
         """ Renvoie le temps de séjour de self """
-        pass
+        return self.fin - self.arrivee
     
     def execution(self):
         """ Renvoie le temps d'exécution de self """
-        pass
+        return self.duree - self.duree_restante
     
     def attente(self):
         """ Renvoie le temps d'attente de self """
-        pass
+        return self.sejour() - self.execution()
     
     def latence(self):
         """ Renvoie le temps de latence de self """
-        pass
+        return self.debut - self.arrivee
 
 class Chronogramme:
     """Représente un chronogramme"""
@@ -52,14 +65,19 @@ class Chronogramme:
         return len(self.table)
     
     def enregistrer(self, p, q):
-        """ int, int -> Nonetype
+        """ Chronogramme, int, int -> Nonetype
         Enregistre l'information : le processus p est exécuté pendant q cycles """
-        pass
+        for _ in range(q):
+            self.table.append(p)
     
     def __repr__(self):
-        """ self -> str
+        """ Chronogramme -> str
         Renvoie une chaine de caractère représentant le chronogramme self """
-        pass
+        # return "|".join([str(p) for p in self.table])
+        s = "|"
+        for p in self.table:
+           s += str(p) + "|"
+        return s
 
 class OrdonnanceurFIFO:
     """Représente un ordonnanceur"""
@@ -75,16 +93,21 @@ class OrdonnanceurFIFO:
         while len(p_list) != 0:
             self.file_processus.enfiler(renvoie_et_supprime_premier(p_list))
         
-    
     def exec_quantum(self):
         """ OrdonnanceurFIFO -> Nonetype
         Simule l'exécution d'un quantum """
-        pass
+        p = self.file_processus.examine()
+        p.avancer(self.quantum, self.chrono.temps_ecoule())
+        self.chrono.enregistrer(p, self.quantum)
+        if p.est_termine():
+            self.file_processus.defiler()
     
     def exec(self):
         """ OrdonnanceurFIFO -> Chronogramme
         Simule l'exécution des processus de la file et en renvoie le chronogramme """
-        pass
+        while not self.file_processus.est_vide():
+            self.exec_quantum()
+        return self.chrono
 
 def renvoie_et_supprime_premier(p_list):
     """ [Processus] -> Processus
@@ -109,5 +132,7 @@ p1 = Processus(1, 0, 10)
 p2 = Processus(2, 3, 4)
 p3 = Processus(3, 2, 8)
 l = [p1,p2,p3]
-        
-
+ordo = OrdonnanceurFIFO()
+ordo.ajoute_processus(l)
+#for _  in range(3):
+#    ordo.exec_quantum()
