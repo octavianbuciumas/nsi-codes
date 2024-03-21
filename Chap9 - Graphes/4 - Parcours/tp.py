4aabf175648e6e6c97a92903d2baba2a3e7c0c15
Florian Picard <florian.picard.fr@gmail.com>
2024-03-21 15:48:56 +0100

    fin tp4

--- Chap9 - Graphes/4 - Parcours/tp.py
+++ Chap9 - Graphes/4 - Parcours/tp.py
@@ -116,25 +116,51 @@ def parcours_largeur(G, depart):
     Parcours le graphe G depuis le sommet depart en largeur
     (sachant que la liste des sommets présents dans vus ont déjà été visités). """
     vus = []
-    à_explorer = ...
-    ...
-    while ...:
-        sommet = ...
-        if sommet in vus:
-            continue
-        # opération de traitement
-        vus.append(sommet)
-        # ajout des voisins non visités
-        for destination in G.voisins(sommet):
-            if ...:
-                ...
-    return vus
+    à_explorer = File()
+    à_explorer.enfiler(depart)
+    while not à_explorer.est_vide():
+        sommet = à_explorer.defiler()
+        # if sommet in vus:
+        #     continue
+        # pareil mais sans "continue"
+        if not sommet in vus:
+            # opération de traitement
+            vus.append(sommet)
+            # ajout des voisins non visités
+            for destination in G.voisins(sommet):
+                if not destination in vus:
+                    à_explorer.enfiler(destination)
+    return vus, à_explorer
 
 
 class MonTableau:
     def __init__(self, tab):
         """ MonTableau, [int] -> Nonetype """
         self.contenu = tab
+        
+    def __contains__(self, elt):
+        """ MonTableau, int -> bool """
+        # pas le droit au mot clé
+        # in
+        # pourt tester l'appartenance ici !
+        # les boucles sont autorisées
+        for e in self.contenu:
+            if e == elt:
+                return True
+        return False
+        # la complexité est LINÉAIRE : on
+        # parcours le tableau dans le pire des cas
+
+# t = MonTableau([1, 2])
+# >>> 1 in t
+# True
+# Opération "in" est linéaire en la taille du tableau
+
+# >>> dico = {1:'a'}
+# >>> 1 in dico
+# True
+# Opération "in" est en temps constant en la taille du dictionnaire
+# = tps indépendant de la taille
 
 class Ensemble:
     def __init__(self):
@@ -143,23 +169,66 @@ class Ensemble:
 
     def ajouter(self, elt):
         """ Ensemble, elt -> Nonetype """
-        pass
+        self.contenu[elt] = True
 
     def supprimer(self, elt):
         """ Ensemble, elt -> Nonetype """
-        pass
+        if elt in self.contenu:
+            # in s'applique à un dictionnaire
+            self.contenu.pop(elt)
 
     def __contains__(self, elt):
         """ Ensemble, elt -> bool """
-        pass
+        if elt in self.contenu:
+            # in s'applique à un dictionnaire
+            return self.contenu[elt]
+        return False
+    # Ensemble supporte "in" en temps constant
+    # elt in e <=> e.__contains__(elt)
 
 def parcours_profondeur_mieux(G, depart):
-    """ Graphe, Sommet -> [Sommet]
+    """ Graphe, Sommet -> {Sommet}
     Parcours le graphe G depuis le sommet depart en profondeur """
-    pass
+    """ Graphe, Sommet, [Sommet] -> [Sommet]
+    Parcours le graphe G depuis le sommet depart en profondeur
+    (sachant que la liste des sommets présents dans vus ont déjà été visités). """
+    vus = Ensemble()
+    ordre = []
+    à_explorer = Pile()
+    à_explorer.empiler(depart)
+    while not à_explorer.est_vide():
+        sommet = à_explorer.depiler()
+        if sommet in vus: # test en temps constant !!!
+            # avant : test en temps linéaire
+            continue
+        vus.ajouter(sommet)
+        ordre.append(sommet) # coute 1 opération
+        # ajout des voisins non visités
+        for destination in reversed(G.voisins(sommet)):
+            if not destination in vus:
+                # on passe d'un test en temps
+                # linéaire à un test en temps
+                # constant car vus est un Ensemble!
+                # C'est fou !
+                à_explorer.empiler(destination)
+    return ordre
 
 def parcours_largeur_mieux(G, depart):
     """ Graphe, Sommet -> [Sommet]
     Parcours le graphe G depuis le sommet depart en largeur """
-    pass
-
+    vus = Ensemble()
+    ordre = []
+    à_explorer = File()
+    à_explorer.enfiler(depart)
+    while not à_explorer.est_vide():
+        sommet = à_explorer.defiler()
+        if sommet in vus: # temps constant
+             continue
+        # opération de traitement
+        vus.ajouter(sommet) # temps constant
+        ordre.append(sommet) # temps constant
+        # ajout des voisins non visités
+        for destination in G.voisins(sommet):
+            if not destination in vus: # temps constant
+                à_explorer.enfiler(destination)
+    return ordre
