30e78b53734d622a5c7d159da1f877ccc1dbff1a
octavianbuciumas <159456327+octavianbuciumas@users.noreply.github.com>
2024-04-27 17:05:12 +0200

    Update 24_NSI_02.py

--- 0 - Épreuve pratique/2024/Sujets/24_NSI_02.py
+++ 0 - Épreuve pratique/2024/Sujets/24_NSI_02.py
@@ -1,17 +1,27 @@
+# Sujet n°02
+# Exercice 1
+
+def correspond(mot, mot_a_trous):
+    return len(mot) == len(mot_a_trous)
+
+# Exercice 2
+
 def est_cyclique(plan):
-    '''Prend en paramètre un dictionnaire `plan` correspondant à 
+    '''Prend en paramètre un dictionnaire `plan` correspondant à
     un plan d'envoi de messages (ici entre les personnes A, B, C,
     D, E, F).
-    Renvoie True si le plan d'envoi de messages est cyclique et 
+    Renvoie True si le plan d'envoi de messages est cyclique et
     False sinon.'''
     expediteur = 'A'
-    destinataire = plan[...] 
+    destinataire = plan[expediteur]
     nb_destinataires = 1
-
+    
     while destinataire != expediteur:
-        destinataire = ... 
-        nb_destinataires = ... 
+        destinataire = plan[destinataire]
+        nb_destinataires = nb_destinataires + 1
+        
+    return nb_destinataires == len(plan)
 
-    return nb_destinataires == ... 
+    
 
 
